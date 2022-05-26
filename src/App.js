41330c35b4826062be7d047debbc98b645979501
coolistkovbel <coolistkovbel@gmail.com>
2022-05-26 18:02:04 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { WagmiConfig, createClient } from 'wagmi'
+
+import Profile from './components/Profile'
+
+const client = createClient()
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <WagmiConfig client={client}>
+      <Profile />
+    </WagmiConfig>
+  )
 }
-
-export default App;
