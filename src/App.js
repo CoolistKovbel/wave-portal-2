d9145322d30669e6b7184e44dc7e1a9e40bb73bc
coolistkovbel <coolistkovbel@gmail.com>
2022-05-26 18:20:40 -0400

    added more feature

--- src/App.js
+++ src/App.js
@@ -7,38 +7,50 @@ import Profile from './components/Profile'
 const client = createClient()
 
 function App() {
-  const [pAccount, setPAccount] = useState("")
+  const [currentAccount, setCurrentAccount] = useState("")
 
-  const getEthereumWallet = async () => {
+  const checkIfWalletIsConnected = async () => {
+    try{
+      const { ethereum } = window;
 
+    if (!ethereum){
+      console.log("Wallet not connected")
+    } else {
+      console.log("Wallet connected", ethereum)
+    }
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account)
+    } else {
+      console.log("No authorized account found")
+    }
+  } catch (error){
+    console.log(error)
+  }
+  }
+
+  const connectWallet = async () => {
     try {
+      const { ethereum } = window;
 
-      const {ethereum} = window
-
-      if(ethereum) {
-        console.log("There is an object ", ethereum)
-      } else{
-        console.log("There is no account")
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
       }
 
-      const accounts = await ethereum.request({method: "eth_accounts"})
-      if(accounts.length !== 0) {
-        const account = accounts[0]
-        console.log("Authorized account: ", account)
-        setPAccount(account)
-      }else{
-        console.log("Not auth account")
-      }
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
 
-
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
     } catch (error) {
       console.log(error)
     }
-
   }
 
   useEffect(() => {
-    getEthereumWallet()
+    checkIfWalletIsConnected()
   })
 
 
@@ -47,7 +59,8 @@ function App() {
     <WagmiConfig client={client}>
       <Profile />
       <div>
-        <h2>Account: {pAccount}</h2>
+        <h2>Account: {currentAccount}</h2>
+        <button onClick={connectWallet}>Connect</button>
       </div>
     </WagmiConfig>
   )
